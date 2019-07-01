234417e09f82ea976867adc80c8fa1e080f6e78a
Catalin Sandu <sandu.catalin@earthlink.ro>
2019-07-01 16:51:04 +0300

    updates AppServiceProvider

--- src/AppServiceProvider.php
+++ src/AppServiceProvider.php
@@ -10,8 +10,22 @@ class AppServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->load()
+            ->observe();
+    }
+
+    private function load()
+    {
         $this->loadRoutesFrom(__DIR__ . '/routes/api.php');
         $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+
+        return $this;
+    }
+
+    private function observe()
+    {
         Currency::observe(Observer::class);
+
+        return $this;
     }
 }
