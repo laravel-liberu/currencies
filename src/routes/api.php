9505385f870bd85b862ca9ccacbc38a571fb29e4
Adrian Ocneanu <aocneanu@gmail.com>
2019-07-02 09:30:21 +0300

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -20,19 +20,20 @@ Route::middleware(['web', 'auth', 'core'])
 
                 Route::get('options', 'Options')->name('options');
             });
+
         Route::namespace('ExchangeRates')
-                ->prefix('exchangeRates')->as('exchangeRates.')
-                ->group(function () {
-                    Route::get('create', 'Create')->name('create');
-                    Route::post('', 'Store')->name('store');
-                    Route::get('{exchangeRate}/edit', 'Edit')->name('edit');
-                    Route::patch('{exchangeRate}', 'Update')->name('update');
-                    Route::delete('{exchangeRate}', 'Destroy')->name('destroy');
+            ->prefix('exchangeRates')->as('exchangeRates.')
+            ->group(function () {
+                Route::get('create', 'Create')->name('create');
+                Route::post('', 'Store')->name('store');
+                Route::get('{exchangeRate}/edit', 'Edit')->name('edit');
+                Route::patch('{exchangeRate}', 'Update')->name('update');
+                Route::delete('{exchangeRate}', 'Destroy')->name('destroy');
 
-                    Route::get('initTable', 'InitTable')->name('initTable');
-                    Route::get('tableData', 'TableData')->name('tableData');
-                    Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
+                Route::get('initTable', 'InitTable')->name('initTable');
+                Route::get('tableData', 'TableData')->name('tableData');
+                Route::get('exportExcel', 'ExportExcel')->name('exportExcel');
 
-                    Route::get('options', 'Options')->name('options');
-                });
+                Route::get('options', 'Options')->name('options');
+            });
     });
