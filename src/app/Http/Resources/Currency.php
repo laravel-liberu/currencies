36bf07d8dbe9d13a69287e4cbf834e081d9d130c
Adrian Ocneanu <aocneanu@gmail.com>
2019-07-03 10:43:50 +0300

    adds currency resource

---
+++ src/app/Http/Resources/Currency.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace LaravelEnso\Currencies\app\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class Currency extends JsonResource
+{
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'symbol' => $this->symbol,
+            'isDefault' => $this->is_default,
+        ];
+    }
+}
