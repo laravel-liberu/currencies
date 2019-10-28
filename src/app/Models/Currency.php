da0ceceacc735a9bb602ccf871c3d67a5bde03b1
mishu <mishu@xtelecom.ro>
2019-10-28 15:14:57 +0200

    adds short name in form

--- src/app/Models/Currency.php
+++ src/app/Models/Currency.php
@@ -10,7 +10,7 @@ class Currency extends Model
 {
     use AvoidsDeletionConflicts, TableCache;
 
-    protected $fillable = ['name', 'symbol', 'is_default'];
+    protected $fillable = ['short_name', 'name', 'symbol', 'is_default'];
 
     protected $casts = ['is_default' => 'boolean'];
 
@@ -18,7 +18,7 @@ class Currency extends Model
     {
         return $query->whereIsDefault(true);
     }
-    
+
     public function scopeForeign($query)
     {
         return $query->whereIsDefault(false);
