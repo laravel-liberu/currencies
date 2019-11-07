dcc2274f373bc860ab658a5ad86e683471394208
mishu <mishu@xtelecom.ro>
2019-11-07 15:48:01 +0200

    fixes link in config;

--- src/config/currencies.php
+++ src/config/currencies.php
@@ -2,7 +2,8 @@
 
 return [
     'fixerCurrencyApi' => [
-        'host' => 'fixer-fixer-currency-v1.p.rapidapi.com',
+        'host' => 'https://fixer-fixer-currency-v1.p.rapidapi.com
+',
         'key' => env('FIXER_CURRENCY_API_KEY', null),
     ],
 ];
