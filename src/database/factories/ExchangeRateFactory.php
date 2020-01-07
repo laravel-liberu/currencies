af2f4a30b523729901c2fa382b72f9627a9f8097
Adrian Ocneanu <aocneanu@gmail.com>
2020-01-07 11:41:57 +0000

    Apply fixes from StyleCI

--- src/database/factories/ExchangeRateFactory.php
+++ src/database/factories/ExchangeRateFactory.php
@@ -4,9 +4,9 @@ use Faker\Generator as Faker;
 use LaravelEnso\Currencies\App\Models\Currency;
 use LaravelEnso\Currencies\App\Models\ExchangeRate;
 
-$factory->define(ExchangeRate::class, fn(Faker $faker) => [
-    'from_id' => fn() => factory(Currency::class)->create()->id,
-    'to_id' => fn() => factory(Currency::class)->create()->id,
+$factory->define(ExchangeRate::class, fn (Faker $faker) => [
+    'from_id' => fn () => factory(Currency::class)->create()->id,
+    'to_id' => fn () => factory(Currency::class)->create()->id,
     'conversion' => $faker->randomFloat(4, 0.0001, 10.0000),
     'date' => now()->subDays(rand(15, 40)),
 ]);
